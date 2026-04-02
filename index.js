3829a44362670395f5f9a1280e117eecfc7831d9
jhardaine <jhardaine177@gmail.com>
2026-04-02 01:18:21 -0400

    n

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-function openMenu() {
-    document.body.classlist += " menu--open"
+function openMenu () {
+    document.body.classList.add ("menu--open")
 }
 
 function closeMenu () {
-    document.body.classlist.remove('menu--open')
+    document.body.classList.remove ('menu--open')
 }
